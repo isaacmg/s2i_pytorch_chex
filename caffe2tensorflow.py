508cf9009c69de881697a24194ee4cfb2552e002
isaacmg <igodfried@isaac26.com>
2018-10-05 17:18:56 -0400

    testing tensorflow

--- caffe2tensorflow.py
+++ caffe2tensorflow.py
@@ -1,7 +1,23 @@
 from onnx_tf.backend import prepare
 import onnx 
+import tensorflow as tf
+import os
+import sys
+from tensorflow.python.platform import gfile
+import numpy as np
+from scipy.misc import imread
+import glob
 def convert_to_tf(onnx_model_path, export_path, device):
     chex_model = onnx.load("model_weights/chexnet.onnx")
     chex_model_tf = prepare(chex_model, device='cpu')
     chex_model_tf.export_graph('model_weights/chest.pb')
-convert_to_tf("", "", "") 
+def test_tensorflow():
+    with gfile.FastGFile("model_weights/chest.pb", 'rb') as f:
+        graph_def = tf.GraphDef()
+        graph_def.ParseFromString(f.read())
+        with tf.Session() as sess:
+            sess.graph.as_default()
+            tf.import_graph_def(graph_def)
+
+#convert_to_tf("", "", "") 
+#test_tensorflow()
---
+++ pytorch_serve_test.py
@@ -0,0 +1,38 @@
+import sys 
+sys.path.append("..")
+from agnostic_model import ModelAgnostic
+import torch
+from collections import OrderedDict
+class PytorchModel(ModelAgnostic):
+    def __init__(self, weight_path, load_type=None):
+        self.torch = __import__('torch')
+        super().__init__(weight_path, "PyTorch")
+        if load_type is "full":
+            self.model = torch.load(weight_path)
+            
+        else:
+            self.model = self.create_model()
+
+            checkpoint = torch.load(weight_path, map_location= lambda storage, loc: storage)
+            new_state_dict = OrderedDict()
+            if torch.cuda.device_count() < 1 and "state_dict" in checkpoint:
+                for k, v in checkpoint['state_dict'].items() :
+                    k = k[7:] # remove `module.`
+
+                
+                self.model.load_state_dict(new_state_dict)
+            # Here we are assuming that raw state_dict has already been transformed 
+            elif "state_dict" not in checkpoint:
+                self.model.load_state_dict(checkpoint)
+            else:
+                self.model = torch.nn.DataParallel(self.model)
+                self.model.load_state_dict(checkpoint['state_dict'])
+
+    def create_model(self):
+        pass 
+
+    def predict(self, formatted_data):
+        self.model(formatted_data)
+    
+    def preprocessing(self, items):
+        pass
